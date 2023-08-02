195f0234016b71a68aa0188dc520c5a5ed6e7fdc
Александр <alteleg@yandex.ru>
2023-08-02 23:01:31 +0300

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Card from './Card';
 import CardImg from './CardImg';
 
 const reactImgSrc = 'https://static10.tgcnt.ru/posts/_0/c8/c8f7f4fa968020aa5670c7d1c00fc320.jpg'
+
 function App(): JSX.Element {
   return (
       <Card>
